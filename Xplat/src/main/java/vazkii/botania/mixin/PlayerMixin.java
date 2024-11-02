e64c5d1bd948dad62d72c58e6120f8226a6fc0b9
Louis Caron <megadoxs@gmail.com>
2024-11-02 00:56:20 -0400

    removing useless imports

--- Xplat/src/main/java/vazkii/botania/mixin/PlayerMixin.java
+++ Xplat/src/main/java/vazkii/botania/mixin/PlayerMixin.java
@@ -1,27 +1,21 @@
 package vazkii.botania.mixin;
 
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import vazkii.botania.api.item.AncientWillContainer;
-import vazkii.botania.common.BotaniaStats;
 import vazkii.botania.common.PlayerAccess;
-import vazkii.botania.common.entity.BotaniaEntities;
 import vazkii.botania.common.item.ResoluteIvyItem;
 
 @Mixin(Player.class)
