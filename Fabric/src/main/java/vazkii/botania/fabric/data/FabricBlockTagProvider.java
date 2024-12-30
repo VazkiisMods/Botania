120a7bbb46d90884389ed40eb767045c96ea13cc
Wormbo <TheRealWormbo@users.noreply.github.com>
2024-12-31 00:55:56 +0100

    Remove unused import in FabricBlockTagProvider.java

--- Fabric/src/main/java/vazkii/botania/fabric/data/FabricBlockTagProvider.java
+++ Fabric/src/main/java/vazkii/botania/fabric/data/FabricBlockTagProvider.java
@@ -6,7 +6,6 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
