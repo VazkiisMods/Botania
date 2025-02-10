2d71a3624b494774de57c7d46428ec16713b918d
jtuc <jtuc@users.noreply.github.com>
2025-02-10 14:29:41 +0000

    Run Advancement triggers for first manaTablet
    The manaTablet is used in recipe unlock advancements, so we should let
    it run triggers at least once per player.

--- Xplat/src/main/java/vazkii/botania/mixin/ServerPlayerMixin.java
+++ Xplat/src/main/java/vazkii/botania/mixin/ServerPlayerMixin.java
@@ -14,11 +14,13 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import vazkii.botania.common.handler.EquipmentHandler;
+import vazkii.botania.common.item.BotaniaItems;
 import vazkii.botania.common.world.SkyblockWorldEvents;
 import vazkii.botania.xplat.XplatAbstractions;
 
@@ -43,8 +45,16 @@ public class ServerPlayerMixin {
 
 	@Mixin(targets = "net/minecraft/server/level/ServerPlayer$2")
 	public static class ContainerListenerMixin {
+		@Unique
+		private boolean botania$seenManaTablet;
+
 		@Inject(at = @At("HEAD"), method = "slotChanged", cancellable = true)
 		private void onSlotChanged(AbstractContainerMenu containerMenu, int slotIndex, ItemStack itemStack, CallbackInfo ci) {
+			if (!botania$seenManaTablet && itemStack.is(BotaniaItems.manaTablet)) {
+				// This might be the player's first mana tablet - let recipe advancement triggers run
+				botania$seenManaTablet = true;
+				return;
+			}
 			// ManaItems update frequently - skip advancement processing for them
 			if (XplatAbstractions.INSTANCE.findManaItem(itemStack) != null) {
 				ci.cancel();
